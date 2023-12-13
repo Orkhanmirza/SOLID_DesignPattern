a70a6263938d58915d9af2bb9bdb2629de2a7696
orxan.mirzayev <orxan.mirzayev@cybernet.az>
2023-12-13 11:18:08 +0400

    added new line SingletonClass

--- src/design_pattern/singleton/SingletonClass.java
+++ src/design_pattern/singleton/SingletonClass.java
@@ -7,9 +7,13 @@ public class SingletonClass {
     private SingletonClass(){
     }
 
-    public static  SingletonClass getInstance(){
-        if (singleton==null) {
-            singleton=new SingletonClass();
+    public static  SingletonClass getInstance() {
+        if (singleton == null) {
+            synchronized (SingletonClass.class) {
+                if (singleton == null) {
+                    singleton = new SingletonClass();
+                }
+            }
         }
         return singleton;
     }
